4a00781564dc85b8695216446f3c3ab5a13d3571
IntelligentBug <85557717+IntelligentBug@users.noreply.github.com>
2021-08-04 16:37:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,40 @@
-# Imports
 import discord
 from discord.ext import commands
 
-# Global Variables
-Prefix = "."
-Token = "pest-your-token-here-and-don't-shere-your-token-with-anyone"
+# Global variables
+Token = "Your-Toke-here"
+Prefix = "b!"
+noob = commands.Bot(command_prefix=Prefix)
+badwords = ['bad', 'words', 'here']
+CH_ID = "Channel Id here"
 
-# Making the Main Bot
-YoungBug = commands.Bot(command_prefix=Prefix)
-
-# Adding a Event To it
-@BlueBug.event
+# Simple Event
+@noob.event
 async def on_ready():
-# Changing The Presence Of Bot
-    await YoungBug.change_presence(activity=discord.Game("Hello There"))
-    print('Done!')
+    await noob.change_presence(activity=discord.Game("WoW"))
+    print('Yeah Boi!')
 
-# Making The Bot Alive
-YoungBug.run(Token)
+# moderation command (links remover and Words remover)
+@noob.event
+async def on_message(message):
+    if 'https://' in message.content:
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} Don't send links to the server")
+
+    for i in badwords:
+        if i in message.content:
+            await message.delete()
+            await message.channel.send(f"{message.author.mention} Don't use this type of language")
+            noob.dispatch('profanity', message, i)
+            return
+        await noob.process_commands(message)
+
+# Logging the words to the channel
+@noob.event
+async def on_profanity(message, word):
+    channel = noob.get_channel(CH_ID)
+    emd = discord.Embed(description=f'{message.author.name} Just said || {word} ||', colour=discord.Color.random())
+    await channel.send(embed=emd)
+
+# running the bot
+noob.run(Token)
